a831280fb8ca877635fb40e4625451cc013e8154
= <=>
2022-11-03 16:40:30 +0800

    [feat] add std,cout,cin区别

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,12 @@
-#include <cstdio>
+#include <iostream>
+using namespace std;
 int main()
 {
-	printf("hello");
+	int a;
+	// cin 其实应该是std::cin
+	// cin、cout、endl 是 std空间中的函数名、因此使用cin等的时候必须有使用std命名空间的说明
+	// 两种方法：直接调用std::cin  | 提前声明 using namespace std; 
+	std::cin >> a; 
+	std::cout << a << std::endl; // endl是换行
+
 }
